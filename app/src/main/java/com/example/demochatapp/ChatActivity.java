5110b1810a3b02cfa3ab815144bc7c69ec26858b
slr <nilaygupta1998@gmail.com>
2020-09-30 04:11:14 +0530

    fetching all contacts

--- app/src/main/java/com/example/demochatapp/ChatActivity.java
+++ app/src/main/java/com/example/demochatapp/ChatActivity.java
@@ -1,14 +1,23 @@
 package com.example.demochatapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
@@ -19,12 +28,15 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
-    private Socket mSocket;
-    private final String TAG="MainActivity";
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    private final String TAG="MainActivity";
+    private Socket mSocket;
     {
         try {
             mSocket = IO.socket("http://192.168.0.104:3000");
@@ -32,53 +44,89 @@ public class ChatActivity extends AppCompatActivity {
             Log.e(TAG, "instance initializer: ERROR");
         }
     }
-    private TextView textView;
-    private Button button;
-    private EditText reveiver_et;;
+    private HashMap<String,String> contacts=new HashMap<>();
+    private String user_email;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        textView=findViewById(R.id.text_tv);
-        button=findViewById(R.id.send_button);
-        reveiver_et=findViewById(R.id.receiver_et);
         final Intent intent=getIntent();
-        final String email=intent.getStringExtra("email");
-        textView.setText(email);
+        user_email=intent.getStringExtra("email");
+        getContactsList();
+    }
 
-        mSocket.connect();
-        mSocket.emit("join",email);
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void getContactsList()
+    {
+        if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        else
+            getContacts();
+    }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String receiver=reveiver_et.getText().toString();
-                mSocket.emit("messagedetection",email,receiver,"Hello");
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! do the
+                    // calendar task you need to do.
+                    Toast.makeText(this,"Request Denied",Toast.LENGTH_SHORT).show();
+                    getContacts();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this,"Request Denied",Toast.LENGTH_SHORT).show();
+                }
+                return;
             }
-        });
+        }
+    }
 
-        mSocket.on("messageToUser", new Emitter.Listener() {
-            @Override
-            public void call(final Object... args) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        JSONObject data = (JSONObject) args[0];
-                        try
-                        {
-                            String nickName=data.getString("sender");
-                            String msg=data.getString("message");
-                            textView.setText(nickName+" : "+msg);
-                            Log.e(TAG, "sender: "+nickName);
-                            Log.e(TAG, "Message: "+msg);
-                        }
-                        catch (JSONException e) {
-                            e.printStackTrace();
+    private void getContacts()
+    {
+        Cursor cursor_Android_Contacts = null;
+        ContentResolver contentResolver = getContentResolver();
+        try {
+            cursor_Android_Contacts = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        } catch (Exception ex) {
+            Log.e("Error on contact", ex.getMessage());
+        }
+
+        if (cursor_Android_Contacts.getCount() > 0)
+        {
+            while (cursor_Android_Contacts.moveToNext()) {
+                String contact_id = cursor_Android_Contacts.getString(cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts._ID));
+                String contact_display_name = cursor_Android_Contacts.getString(cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                int hasPhoneNumber = Integer.parseInt(cursor_Android_Contacts.getString(cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                if (hasPhoneNumber > 0)
+                {
+                    Cursor phoneCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                            , null
+                            , ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?"
+                            , new String[]{contact_id}
+                            , null);
+
+                    while (phoneCursor.moveToNext())
+                    {
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        phoneNumber=phoneNumber.replaceAll("\\s", "");
+                        if(phoneNumber.length()>=10) {
+                            phoneNumber=phoneNumber.substring(phoneNumber.length()-10,phoneNumber.length());
+                            contacts.put(phoneNumber,contact_display_name);
                         }
                     }
-                });
+                    phoneCursor.close();
+                }
             }
-        });
+        }
+        for(Map.Entry<String,String> entry:contacts.entrySet())
+            Log.e(TAG, entry.getKey()+" : "+entry.getValue());
     }
 }
---
+++ app/src/main/java/com/example/demochatapp/socketCode.java
@@ -0,0 +1,46 @@
+package com.example.demochatapp;
+
+import android.util.Log;
+import android.view.View;
+
+import com.github.nkzawa.emitter.Emitter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class socketCode {
+
+//    mSocket.connect();
+//    mSocket.emit("join",user_email);
+//    button.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            String receiver=reveiver_et.getText().toString();
+//            mSocket.emit("messagedetection",email,receiver,"Hello");
+//        }
+//    });
+//
+//        mSocket.on("messageToUser", new Emitter.Listener() {
+//        @Override
+//        public void call(final Object... args) {
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    JSONObject data = (JSONObject) args[0];
+//                    try
+//                    {
+//                        String nickName=data.getString("sender");
+//                        String msg=data.getString("message");
+//                        textView.setText(nickName+" : "+msg);
+//                        Log.e(TAG, "sender: "+nickName);
+//                        Log.e(TAG, "Message: "+msg);
+//                    }
+//                    catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
+//        }
+//    });
+
+}
